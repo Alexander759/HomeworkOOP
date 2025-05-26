a930eee530fa1a9e109c92e4faf1eae51875101f
Alexander759 <aleks.asenov2005@gmail.com>
2025-05-27 00:59:41 +0300

    Add dynamic list implementation

--- HomeworkOOP/HomeworkOOP.cpp
+++ HomeworkOOP/HomeworkOOP.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include "MyString.h"
+#include "List.hpp"
 
 int main()
 {
+	List<List<int>> l;
+
 	MyString m = "j";
 	m += "new";
 	std::cout << m;
---
+++ HomeworkOOP/List.hpp
@@ -0,0 +1,221 @@
+#include <stdexcept>
+
+template <class T>
+class List {
+public:
+	List();
+	List(const List<T>& other);
+	List(List<T>&& other);
+	List<T>& operator=(const List<T>& other);
+	List<T>& operator=(List<T>&& other);
+	~List();
+
+	int indexOf(const T& item) const;
+	void add(const T& item);
+	void removeAt(size_t index);
+	void remove(const T& item);
+	size_t getLength() const;
+	
+	bool operator==(const List<T>& other) const;
+	const T& operator[](size_t index) const;
+	T& operator[](size_t index);
+private:
+	static double resizeFactor;
+	static size_t defaultStartCapacity;
+	T* content;
+	size_t length;
+	size_t capacity;
+
+	void copyFrom(const List<T>& other);
+	void free();
+	void resizeUp();
+	void resizeDown();
+};
+
+template<class T>
+double List<T>::resizeFactor = 1.5;
+
+template<class T>
+size_t List<T>::defaultStartCapacity = 8;
+
+template<class T>
+inline List<T>::List() : length(0), capacity(defaultStartCapacity) {
+	this->content = new T[this->capacity];
+}
+
+template<class T>
+inline List<T>::List(const List<T>& other) {
+	copyFrom(other);
+}
+
+template<class T>
+inline List<T>::List(List<T>&& other) {
+	this->length = other.length;
+	this->capacity = other.capacity;
+	this->content = other.content;
+	other.content = nullptr;
+	other.length = 0;
+	other.capacity = 0;
+}
+
+template<class T>
+inline List<T>& List<T>::operator=(const List<T>& other) {
+	if (this != &other) {
+		this->free();
+		this->copyFrom(other);
+	}
+
+	return *this;
+}
+
+template<class T>
+inline List<T>& List<T>::operator=(List<T>&& other) {
+	if (this != &other) {
+		this->free();
+		this->length = other.length;
+		this->capacity = other.capacity;
+		this->content = other.content;
+		other.content = nullptr;
+		other.length = 0;
+		other.capacity = 0;
+	}
+
+	return *this;
+}
+
+template<class T>
+inline List<T>::~List() {
+	this->free();
+}
+
+template<class T>
+inline int List<T>::indexOf(const T& item) const {
+	for (size_t i = 0; i < this->length; i++) {
+		if (item == this->content[i]) {
+			return i;
+		}
+	}
+
+	return -1;
+}
+
+template<class T>
+inline void List<T>::add(const T& item) {
+	if (this->length == this->capacity) {
+		this->resizeUp();
+	}
+
+	this->content[this->length] = item;
+	this->length++;
+}
+
+template<class T>
+inline void List<T>::removeAt(size_t index) {
+	if (index >= length) {
+		throw std::out_of_range("Index out of range");
+	}
+	
+	for (size_t i = index; i < this->length - 1; i++) {
+		this->content[i] = this->content[i + 1];
+	}
+	this->length--;
+
+	if (static_cast<size_t>(this->capacity / this->resizeFactor) > this->length) {
+		resizeDown();
+	}
+}
+
+template<class T>
+inline void List<T>::remove(const T& item) {
+	int index = this->indexOf(item);
+	if (index == -1) {
+		return;
+	}
+
+	this->removeAt(index);
+}
+
+template<class T>
+inline size_t List<T>::getLength() const {
+	return this->length;
+}
+
+template<class T>
+inline bool List<T>::operator==(const List<T>& other) const {
+	if (this == &other) {
+		return true;
+	}
+
+	if (this->length != other.length) {
+		return false;
+	}
+
+	for (size_t i = 0; i < this->length; i++) {
+		if (this->content[i] != other.content[i]) {
+			return false;
+		}
+	}
+
+	return true;
+}
+
+template<class T>
+inline const T& List<T>::operator[](size_t index) const {
+	if (index >= length) {
+		throw std::out_of_range("Index out of range");
+	}
+
+	return this->content[index];
+}
+
+template<class T>
+inline T& List<T>::operator[](size_t index) {
+	if (index >= length) {
+		throw std::out_of_range("Index out of range");
+	}
+
+	return this->content[index];
+}
+
+template<class T>
+inline void List<T>::copyFrom(const List<T>& other) {
+	this->length = other.length;
+	this->capacity = other.capacity;
+	this->content = new T[this->capacity];
+	for (size_t i = 0; i < this->length; i++) {
+		this->content[i] = other.content[i];
+	}
+}
+
+template<class T>
+inline void List<T>::free() {
+	delete[] this->content;
+	this->length = 0;
+	this->capacity = 0;
+}
+
+template<class T>
+inline void List<T>::resizeUp() {
+	this->capacity = static_cast<size_t>(this->capacity * resizeFactor);
+	T* newContent = new T[this->capacity];
+
+	for (size_t i = 0; i < this->length; i++) {
+		newContent[i] = this->content[i];
+	}
+
+	delete[] this->content;
+	this->content = newContent;
+}
+
+template<class T>
+inline void List<T>::resizeDown() {
+	this->capacity = static_cast<size_t>(this->capacity / resizeFactor);
+	T* newContent = new T[this->capacity];
+
+	for (size_t i = 0; i < this->length; i++) {
+		newContent[i] = this->content[i];
+	}
+
+	delete[] this->content;
+	this->content = newContent;
+}
